eaa969c62b1a10ae8fdcc5dc9f77049b7557a23a
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-02 03:58:30 -0500

    WIP

--- test/unit_JS_SPCOIN/index.js
+++ test/unit_JS_SPCOIN/index.js
@@ -7,13 +7,14 @@ const {
    week, 
    year, 
    month, 
-   millennium } = require( "../../node_modules-dev/bg/spcoin-access-modules/utils/dateTime"); 
+   millennium } = require( "../../node_modules-dev/spcoin-back-end/spcoin-access-modules/utils/dateTime"); 
+const JUNK      = require( "../../node_modules-dev/spcoin-back-end/spcoin-access-modules/")
 
    //  millennium } = require( "../../spcoin-access-modules/utils/dateTime"); 
 const { assert } = require ('chai');
 const { HHAccountRateMethods } = require("../lib/hhAccountRateMethods");
 const { deploySpCoinContract, getDeployedArtifactsABIAddress } = require("../lib/deployContract");
-const { SpCoinAccessModules } = require("../../node_modules-dev/bg/spcoin-access-modules/index.js");
+const { SpCoinAccessModules } = require("../../node_modules-dev/spcoin-back-end/spcoin-access-modules/index.js");
 // const { SpCoinAccessModules } = require("@sponsorcoin/spcoin-access-modules/index");
 
 let signer;
