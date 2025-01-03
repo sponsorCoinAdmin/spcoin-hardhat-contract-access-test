242466379e1ca110470581ca25c806b95faa7bd3
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-03 18:39:28 -0500

    WIP

--- scripts/setupEnvAliases.sh
+++ scripts/setupEnvAliases.sh
@@ -1,40 +1,32 @@
-export CURR_DIR=$PWD
-#cd ..
+clear
+echo ==== SETTING UP ENVIRONMENT VARIABLES ====
+export ACTIVE_PROJECT_NAME=$(basename $PWD)
 
 #echo "Configuring SponsorCoin Environment"
-export PROJECT_HOME=$PWD
-export ENV_DIR=$PROJECT_HOME'/.e'
-export LOGS_DIR=$ENV_DIR/logs
+export ACTIVE_PROJECT_PATH=$PWD
+export ACTIVE_ENV_PATH=$ACTIVE_PROJECT_PATH'/.e'
+export LOGS_DIR=$ACTIVE_ENV_PATH/logs
 
-echo PROJECT_HOME=$PROJECT_HOME
-echo ENV_DIR=$ENV_DIR
+echo ACTIVE_PROJECT_PATH=$ACTIVE_PROJECT_PATH
+echo ACTIVE_ENV_PATH=$ACTIVE_ENV_PATH
 echo LOGS_DIR=$LOGS_DIR
 
-echo "Adding startup configuration Files to Sponsor Coin environment setup file $ENV_DIR/.e"
-echo "set -o vi" | tee $ENV_DIR/.e
-echo "export PROJECT_HOME=$PROJECT_HOME" | tee -a $ENV_DIR/.e
-echo "export ENV_DIR=$ENV_DIR" | tee -a $ENV_DIR/.e
-echo "export SPONSOR_COIN_LOG=$LOGS_DIR" | tee -a $ENV_DIR/.e
-echo "export HH_SCRIPTS=$PROJECT_HOME/scripts" | tee -a $ENV_DIR/.e
-echo ". $ENV_DIR/.a" | tee -a $ENV_DIR/.e
-echo m | tee -a $ENV_DIR/.e
+echo "Adding startup configuration Files to Sponsor Coin environment setup file $ACTIVE_ENV_PATH/.e"
+echo "export ACTIVE_PROJECT_PATH=$ACTIVE_PROJECT_PATH" | tee -a $ACTIVE_ENV_PATH/.e
+echo "export ACTIVE_ENV_PATH=$ACTIVE_ENV_PATH"         | tee -a $ACTIVE_ENV_PATH/.e
+echo "export SPONSOR_COIN_LOG=$LOGS_DIR"               | tee -a $ACTIVE_ENV_PATH/.e
+echo "export HH_SCRIPTS=$ACTIVE_PROJECT_PATH/scripts"  | tee -a $ACTIVE_ENV_PATH/.e
+echo ". $ACTIVE_ENV_PATH/.a"                           | tee -a $ACTIVE_ENV_PATH/.e
+# echo m                                                 | tee -a $ACTIVE_ENV_PATH/.e
 
 echo "Adding sponsor coin startup configuration Files to bootstrap file ~/.baschrc"
-echo ". "$ENV_DIR"/.e" | tee -a ~/.bashrc
+sed -i '/ACTIVE_ENV_PATH/d' ~/.bashrc
+echo "export ACTIVE_ENV_PATH=$ACTIVE_ENV_PATH/.e"                                        | tee -a ~/.bashrc
+echo ". \$ACTIVE_ENV_PATH"                                                               | tee -a ~/.bashrc
+
+#echo ". "$ACTIVE_ENV_PATH"/.e" | tee -a ~/.bashrc
 
 echo "Starting The Project Environment"
-. $ENV_DIR/.e
+. $ACTIVE_ENV_PATH/.e
 cd $CURR_DIR
 echo "***IMPORTANT *** Please ensure the '.env' file is configured for proper operations"
-
-#echo "Adding sponsor coin startup configuration Files to bootstrap file ~/.baschrc"
-#echo ". "$ENV_DIR"/.e" | tee -a ~/.bashrc
-#. $ENV_DIR/.e
-#echo "***IMPORTANT *** Please ensure the '.env' file is configured for proper operations"
-
-
-# echo "Adding startup configuration Files to bootstrap file ~/.baschrc"
-# echo "export PROJECT_HOME=$PROJECT_HOME" | tee -a ~/.bashrc
-# echo "export ENV_DIR=$ENV_DIR" | tee -a ~/.bashrc
-# echo "export LOGS_DIR=$LOGS_DIR" | tee -a ~/.bashrc
-
