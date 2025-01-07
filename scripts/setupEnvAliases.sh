0a2d725c46cfde79f12393293d71268a41e74616
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-06 20:16:06 -0500

    WIP

--- scripts/setupEnvAliases.sh
+++ scripts/setupEnvAliases.sh
@@ -1,7 +1,6 @@
 clear
 echo ==== SETTING UP ENVIRONMENT VARIABLES ====
 export ACTIVE_PROJECT_NAME=$(basename $PWD)
-export ACTIVE_ROOT_DIR="$(dirname "$PWD")"
 export ACTIVE_PROJECT_PATH=$PWD
 export ACTIVE_ENV_DIR=/.e
 
@@ -16,8 +15,8 @@ insertOnce() {
 }
 
 createNewEnvironmentFile() {
-    export ACTIVE_PROJECT_PATH=$1
-    export ACTIVE_PROJECT_NAME=$2
+    export ACTIVE_PROJECT_NAME=$1
+    export ACTIVE_PROJECT_PATH=$2
     export ACTIVE_ENV_DIR=$3
     export ACTIVE_ENV_PATH=$ACTIVE_PROJECT_PATH$ACTIVE_ENV_DIR
     export ACTIVE_ENV_FILE_PATH=$ACTIVE_ENV_PATH/.e
@@ -38,7 +37,7 @@ createNewEnvironmentFile() {
 }
 
 #SET UP BASH ENVIRONMENT
-createNewEnvironmentFile $ACTIVE_PROJECT_PATH $ACTIVE_PROJECT_NAME $ACTIVE_ENV_DIR
+createNewEnvironmentFile $ACTIVE_PROJECT_NAME $ACTIVE_PROJECT_PATH $ACTIVE_ENV_DIR
 insertOnce "set -o vi" ~/.bashrc;
 
 echo "Adding sponsor coin startup configuration Files to bootstrap file ~/.bashrc"
