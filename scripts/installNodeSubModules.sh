f366c6a579a1501a580c42cfa77dcf9e9dc9a910
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-13 17:39:58 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,6 +1,6 @@
 clear
 echo "======================spcoin-hardhat-contract-access-test================================="
-PWD
+pwd
 ls
 CURR_DIR=$PWD
 cd ./node_modules-dev/spcoin-common/spcoin-lib
@@ -10,6 +10,6 @@ cd $CURR_DIR
 cd ./node_modules-dev/spcoin-back-end/spcoin-access-modules
 npm i
 cd $CURR_DIR
-cd ./node_modules-dev/spcoin-back-end/spcoin-with-modules
+cd ./node_modules-dev/spcoin-back-end/spcoin-weth-module-cjs
 npm i
 cd $CURR_DIR
