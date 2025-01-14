5aefa6d0770cc8aaa4e357b6664003d8e79d68d2
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-14 05:26:12 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,15 +1,21 @@
+exeIfDirExists() {
+    local CURR_DIR=$PWD
+    DIRECTORY=$1
+    COMMAND=$2
+    echo "FROM ROOT DIR        : $PWD"
+    if [ -d "$DIRECTORY" ]; then
+        echo "INSTALLING MODULES IN: $DIRECTORY"
+        echo "EXECUTING            : $COMMAND"
+    else 
+        echo "***ERROR: DIRECTORY $DIRECTORY not found"
+        echo "***ERROR: COMMAND $COMMAND NOT EXECUTED" 
+    fi
+    cd $CURR_DIR
+}
+
 clear
 echo "======================spcoin-hardhat-contract-access-test================================="
 pwd
-ls
-CURR_DIR=$PWD
-cd ./node_modules-dev/spcoin-common/spcoin-lib
-pwd
-npm i
-cd $CURR_DIR
-cd ./node_modules-dev/spcoin-back-end/spcoin-access-modules
-npm i
-cd $CURR_DIR
-cd ./node_modules-dev/spcoin-back-end/spcoin-weth-module-cjs
-npm i
-cd $CURR_DIR
+exeIfDirExist cd ./node_modules-dev/spcoin-common/spcoin-lib npm i
+exeIfDirExist ./node_modules-dev/spcoin-back-end/spcoin-access-modules npm i
+exeIfDirExist ./node_modules-dev/spcoin-back-end/spcoin-weth-module-cjs npm i
