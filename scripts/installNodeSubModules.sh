1b39a7fdf574af6b320c370e53c1012709d48b51
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-16 01:51:28 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,22 +1,22 @@
 exeIfDirExists() {
-    local CURR_DIR=$PWD
     DIRECTORY=$1
     COMMAND=$2
-    echo "FROM ROOT DIR        : $PWD"
+    local CURR_DIR=$PWD
+    echo "CURRENT DIRECTORY : $PWD"
     if [ -d "$DIRECTORY" ]; then
-        echo "INSTALLING MODULES IN: $DIRECTORY"
-        echo "EXECUTING            : $COMMAND"
-        # $COMMAND
+        echo INSTALLING in: SUB-DIRECTORY $DIRECTORY =====
+        cd $DIRECTORY
+        echo "EXECUTING: $COMMAND"
+        eval $COMMAND
     else 
-        echo "***ERROR: DIRECTORY $DIRECTORY not found"
+        echo "***ERROR: SUB-DIRECTORY $DIRECTORY NOT FOUND"
         echo "***ERROR: COMMAND $COMMAND NOT EXECUTED" 
     fi
     cd $CURR_DIR
 }
 
-clear
-echo "======================spcoin-hardhat-contract-access-test================================="
+echo "===================== spcoin-hardhat-contract-access-test ==============================="
 pwd
-exeIfDirExists "./node_modules-dev/spcoin-common/spcoin-lib" npm i
-exeIfDirExists ./node_modules-dev/spcoin-back-end/spcoin-access-modules npm i
-exeIfDirExists ./node_modules-dev/spcoin-back-end/spcoin-weth-module-cjs npm i
+exeIfDirExists ./node_modules-dev " . ./scripts/installNodeSubModules.sh"
+# exeIfDirExists ./node_modules-dev/spcoin-back-end/spcoin-access-modules "npm i"
+# exeIfDirExists ./node_modules-dev/spcoin-back-end/spcoin-weth-module-cjs "npm i"
